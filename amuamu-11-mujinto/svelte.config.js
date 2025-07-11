dada9de2763f68dbdd138e119c78d3a7f7e53a5b
bleaguedb <mail@bleaguedb.com>
2025-07-11 23:41:28 +0900

    debug github pages settings

--- amuamu-11-mujinto/svelte.config.js
+++ amuamu-11-mujinto/svelte.config.js
@@ -14,7 +14,10 @@ const config = {
 		adapter: adapter({fallback: 'index.html'}),
 		prerender: {
 			entries: ['*']
-		}
+		},
+		paths: {
+      base: process.env.NODE_ENV === 'production' ? '/amuamu' : '',
+    }
 	}
 };
 
